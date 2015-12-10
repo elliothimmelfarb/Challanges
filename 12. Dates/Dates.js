47dc3a082e0e53f6bcaf1b84531aaa1e11b03889
Elliot Himmelfarb <elliothimmelfarb@gmail.com>
2015-12-10 21:27:05 +0900

    Added var months

---
+++ 12. Dates/Dates.js
@@ -0,0 +1,18 @@
+function age(year, month, day) {
+    
+    var d = new Date();
+    
+    var years = Math.abs(d.getFullYear() - year);
+    
+    // var months = 
+    
+    // var days = 
+    
+    // if days more than thirty
+        // add one to month
+        // add remainder to days
+    
+    return "date";
+}
+
+console.log(age(2017, 11, 17));
---
+++ 12. Dates/README.md
@@ -0,0 +1,7 @@
+returns how many years, months, and days the inputed date is from the current date.
+month argument starts from 0 (11 is December)
+
+ex:
+
+
+
