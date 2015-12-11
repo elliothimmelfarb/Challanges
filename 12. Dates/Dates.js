3911c0a75903eaab83db05ce9c4b9bca967c03e1
Elliot Himmelfarb <elliothimmelfarb@gmail.com>
2015-12-11 21:54:15 +0900

    getting closer but months show one extra and days are not working properly

--- 12. Dates/Dates.js
+++ 12. Dates/Dates.js
@@ -1,25 +1,27 @@
 function age(year, month, day) {
     
     // since the date object can find the number of miliseconds between to date objects,
-    // I will use the number of miliseconds in a day to calculate the number of days
     var dayMil = 1000 * 60 * 60 * 24;
     
-    // create new Date object from input perameters
-    var peramDate = new Date(year, month, day);
-    console.log(peramDate);
-    //create new Date object from current day;
+    // create new Date object for today to get year, month, and date from in order get the "apples to apples" date difference 
+    var today = new Date();
     
     // get the difference in miliseconds between the two dates
-    var timeDiff = Math.abs(new Date() - peramDate);
-    console.log(timeDiff);
+    var totalDays = Math.abs((new Date(2015, 3, 2) - new Date(year, month, day)) / dayMil);
+    console.log(totalDays);
     
-    // get the days, months, and years difference
-    var days = Math.floor(timeDiff/dayMil);
-    console.log(days);
-    var months = Math.floor(days/30);
-    var years = Math.floor(months/12);
+    var years = Math.floor(totalDays/365);
+    var months = Math.floor(totalDays/30 - years * 12);
+    var days = Math.floor(totalDays - (years * 365 + months * 30)); // doesnt work properly
+
+    
+    // recalibrate days to account for actual calendar days
+    
     
     return years + " years, " + months + " months, and " + days + " days."; 
 }
 
-console.log(age(2015, 11, 13));
+console.log(age(2017, 11, 17));
+console.log(age(2015, 3, 6));
+
+// today.getFullYear(), today.getMonth(), today.getDate()
