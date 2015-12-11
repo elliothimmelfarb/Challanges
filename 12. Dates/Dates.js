79b880325ad4777170c9a4ed1cab5c05d57d4c7d
Elliot Himmelfarb <elliothimmelfarb@gmail.com>
2015-12-11 18:03:04 +0900

    Got it working again

--- 12. Dates/Dates.js
+++ 12. Dates/Dates.js
@@ -2,15 +2,24 @@ function age(year, month, day) {
     
     // since the date object can find the number of miliseconds between to date objects,
     // I will use the number of miliseconds in a day to calculate the number of days
-    var day = 1000 * 60 * 60 * 24;
+    var dayMil = 1000 * 60 * 60 * 24;
     
     // create new Date object from input perameters
     var peramDate = new Date(year, month, day);
     console.log(peramDate);
     //create new Date object from current day;
-    var today = new Date();
-    console.log(today);
     
+    // get the difference in miliseconds between the two dates
+    var timeDiff = Math.abs(new Date() - peramDate);
+    console.log(timeDiff);
+    
+    // get the days, months, and years difference
+    var days = Math.floor(timeDiff/dayMil);
+    console.log(days);
+    var months = Math.floor(days/30);
+    var years = Math.floor(months/12);
+    
+    return years + " years, " + months + " months, and " + days + " days."; 
 }
 
-console.log(age(2017, 11, 17));
+console.log(age(2015, 11, 13));
