4ee10efbfda6fa57e8661a448c52ea571588e8ba
Elliot Himmelfarb <elliothimmelfarb@gmail.com>
2015-12-18 16:38:17 +0900

    Finished RepeatCounter subfunction

--- 14. Repeated Letters/RepeatedLetters.js
+++ 14. Repeated Letters/RepeatedLetters.js
@@ -15,7 +15,31 @@ function wordSelector(str) {
         var repeatedLetters = "";
         
         // store number of repeats
-        var repeatsNum = 0; 
+        var repeatsNum = 0;
+        
+        // check each char in word
+        for (var i = 0; i < word.length; i++) {
+            
+            // against each other char in word
+            for (var j = 0; j < word.length; j++) {
+                
+                // if the indexes are the same, skip to next index
+                if (i === j) j ++;
+                
+                // if chars match and are not found in the repeatedLetters string
+                if (word.charAt(i) === word.charAt(j) && repeatedLetters.indexOf(word.charAt(i)) === -1) {
+                    
+                    // add char to to repeatedLetters string
+                    repeatedLetters += word.charAt(i);
+                    
+                    //increment repeats counter
+                    repeatsNum++;
+                }
+            }
+        }
+        
+        // return the number of repeats found
+        return repeatsNum;
     }
     
     var highestRepeats = [];
