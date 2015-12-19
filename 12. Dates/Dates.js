474730de8b01b541cf1a4ff8214dda46bccba4ca
Elliot Himmelfarb <elliothimmelfarb@gmail.com>
2015-12-19 16:45:06 +0900

    days difference working. Now must add print and parse statements

--- 12. Dates/Dates.js
+++ 12. Dates/Dates.js
@@ -1,27 +1,20 @@
 function age(year, month, day) {
     
-    // since the date object can find the number of miliseconds between to date objects,
-    var dayMil = 1000 * 60 * 60 * 24;
+    var d = new Date();
     
-    // create new Date object for today to get year, month, and date from in order get the "apples to apples" date difference 
-    var today = new Date();
+    var today = new Date(d.getFullYear(), d.getMonth(), d.getDate());
     
-    // get the difference in miliseconds between the two dates
-    var totalDays = Math.abs((new Date(2015, 3, 2) - new Date(year, month, day)) / dayMil);
-    console.log(totalDays);
+    var targetDate = new Date (year, month, day);
     
-    var years = Math.floor(totalDays/365);
-    var months = Math.floor(totalDays/30 - years * 12);
-    var days = Math.floor(totalDays - (years * 365 + months * 30)); // doesnt work properly
-
+    var diffInMil = Math.abs(today - targetDate);
     
-    // recalibrate days to account for actual calendar days
+    var diffInDays = diffInMil / (1000*60*60*24);
+    
+   // TODO print and parse statements
     
     
-    return years + " years, " + months + " months, and " + days + " days."; 
 }
 
-console.log(age(2017, 11, 17));
-console.log(age(2015, 3, 6));
+console.log(age(2015, 11, 20));
 
 // today.getFullYear(), today.getMonth(), today.getDate()
