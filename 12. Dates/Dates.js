e832614e8c755f8812b56f11b97c79d6c6887e13
Elliot Himmelfarb <elliothimmelfarb@gmail.com>
2015-12-21 19:23:01 +0900

    got years months days working

--- 12. Dates/Dates.js
+++ 12. Dates/Dates.js
@@ -8,11 +8,19 @@ function age(year, month, day) {
     
     var diffInMil = Math.abs(today - targetDate);
     
-    var diffInDays = diffInMil / (1000*60*60*24);
+    var daysDiff = diffInMil / (1000*60*60*24);
     
-   // TODO print and parse statements
+    var years, months, days;
     
+    daysDiff = 789;
     
+    years = Math.floor(daysDiff/365);
+    
+    months = Math.floor((daysDiff - (years * 365)) / 30);
+    
+    days = Math.floor((daysDiff - (years * 365 + months * 30)));
+    
+    console.log(years, " ", months, " ", days);
 }
 
 console.log(age(2015, 11, 20));
